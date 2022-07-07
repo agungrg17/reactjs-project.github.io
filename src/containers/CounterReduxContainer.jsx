eb2e5df8b0cde67f9bbce03dbc427d945e63e739
Agung Rakhmat Gumilar <agungrg17@gmail.com>
2022-07-07 23:49:40 +0700

    made counter with redux

--- src/containers/CounterReduxContainer.jsx
+++ src/containers/CounterReduxContainer.jsx
@@ -1,3 +1,97 @@
-const CounterReduxContainer = () => {};
+import React from "react";
+
+import { Box, Button, TextField, Typography } from "@mui/material";
+
+// Di sini kita akan menggunakan useDispatch dan useSelector
+// useSelector = adalah suatu hooks yang memperbolehkan kita memilih global state yang digunakan
+// useDispatch = adalah suatu hooks yang memperbolehkan kita memilih action apa yang ingin dilakukan
+import { useDispatch, useSelector } from "react-redux";
+
+const CounterReduxContainer = () => {
+  // useSelector ini menerima sebuah fungsi dengan satu parameter "state"
+  // anggap saja ini seperti "filter"
+  const username = useSelector((state) => state.user);
+  const counter = useSelector((state) => state.counter);
+
+  // Untuk menggunakan dispatch (action) nya, kita gunakan dispatcher dari hooks useDispatch
+  const dispatcher = useDispatch();
+
+  const buttonDecrementOnClickHandler = () => {
+    // Di sini kita akan memanggil dispatcher-nya,
+    // jangan lupa untuk melemparkan aksi apa yang ingin dilakukan via
+    // props "type"
+    dispatcher({
+      type: "decrement",
+    });
+  };
+
+  const buttonResetOnClickHandler = () => {
+    dispatcher({
+      type: "reset",
+    });
+  };
+
+  const buttonIncrementOnClickHandler = () => {
+    dispatcher({
+      type: "increment",
+    });
+  };
+
+  return (
+    <>
+      <Box
+        sx={{
+          border: "1px dashed grey",
+          p: 2,
+          display: "flex",
+          flexDirection: "column",
+          gap: 2,
+        }}
+      >
+        <Typography variant="body1" component="div">
+          React Redux
+        </Typography>
+
+        <Typography variant="body1" component="div">
+          Nama User: {username}
+        </Typography>
+
+        <Box sx={{ display: "flex", gap: 2 }}>
+          <TextField
+            disabled
+            label="Current Counter"
+            // defaultValue="0"
+            value={counter}
+            size="small"
+          />
+        </Box>
+
+        <Box sx={{ display: "flex", gap: 2 }}>
+          <Button
+            variant="outlined"
+            color="success"
+            onClick={buttonDecrementOnClickHandler}
+          >
+            -1
+          </Button>
+          <Button
+            variant="outlined"
+            color="success"
+            onClick={buttonResetOnClickHandler}
+          >
+            0
+          </Button>
+          <Button
+            variant="outlined"
+            color="success"
+            onClick={buttonIncrementOnClickHandler}
+          >
+            +1
+          </Button>
+        </Box>
+      </Box>
+    </>
+  );
+};
 
 export default CounterReduxContainer;
--- src/index.js
+++ src/index.js
@@ -1,42 +1,102 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+// Counter redux
 
-import reportWebVitals from './reportWebVitals';
-//import { Provider } from 'react-redux';
-//import store from './app/store';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+
+// Kenapa legacy? nanti kita akan telusuri yah !
+import { legacy_createStore as createStore } from "redux";
+// Jangan lupa import reducernya juga
+import { initialValue, rootReducer } from "./reducers/rootReducer";
+
+// Jangan lupa juga untuk import Provider dari react-redux
+import { Provider } from "react-redux";
+
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import CounterReducerContainer from "./containers/CounterReducerContainer";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+// Apabila pada saat menggunakan useReducer kita membuat dan menggunakan
+// reducernya dari sisi Component yang ingin digunakan, maka pada redux,
+// kita benar benar membuatnya secara "global" di index.js (root Component)
+
+import CounterReducerContainer from "./containers/CounterReducerContainer";
+// Jangan lupa untuk import CounterReduxContainer
+import CounterReduxContainer from "./containers/CounterReduxContainer";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+// di sini kita akan membuat "store"-nya (global state manager-nya)
+const store = createStore(rootReducer, initialValue);
 
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />}>
-          <Route path="use-reducer" element={<CounterReducerContainer />} />
-          <Route
-            path="react-redux"
-            element={
-              <>
-                <h1>React Redux</h1>
-              </>
-            }
-          />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    {/* Di sini kita menggunakan provider */}
+    {/* Provider menerima sebuah attributes bernama "store", 
+        yang valuenya adalah store yang kita buat sebelumnya  */}
+    {/* Mirip dengan Context API pada React */}
+    <Provider store={store}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<App />}>
+            <Route path="use-reducer" element={<CounterReducerContainer />} />
+            {/* Jangan lupa ganti elementnya menjadi CounterReduxContainer */}
+            <Route path="react-redux" element={<CounterReduxContainer />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>
-  // <React.StrictMode>
-  //   <Provider store={store} >
-  //     <App />
-  //   </Provider>
-  // </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+
+
+//Counter with reducer
+
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import './index.css';
+// import App from './App';
+
+// import reportWebVitals from './reportWebVitals';
+// //import { Provider } from 'react-redux';
+// //import store from './app/store';
+// import { BrowserRouter, Routes, Route } from "react-router-dom";
+// import CounterReducerContainer from "./containers/CounterReducerContainer";
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// root.render(
+//   <React.StrictMode>
+//     <BrowserRouter>
+//       <Routes>
+//         <Route path="/" element={<App />}>
+//           <Route path="use-reducer" element={<CounterReducerContainer />} />
+//           <Route
+//             path="react-redux"
+//             element={
+//               <>
+//                 <h1>React Redux</h1>
+//               </>
+//             }
+//           />
+//         </Route>
+//       </Routes>
+//     </BrowserRouter>
+//   </React.StrictMode>
+//   // <React.StrictMode>
+//   //   <Provider store={store} >
+//   //     <App />
+//   //   </Provider>
+//   // </React.StrictMode>
+// );
+
+// // If you want to start measuring performance in your app, pass a function
+// // to log results (for example: reportWebVitals(console.log))
+// // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// reportWebVitals();
