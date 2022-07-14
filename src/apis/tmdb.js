0267aada22ef8825be74e588a67a04dc93dfd772
Agung Rakhmat Gumilar <agungrg17@gmail.com>
2022-07-14 23:09:38 +0700

    try to deploy

--- src/apis/tmdb.js
+++ src/apis/tmdb.js
@@ -1,11 +1,13 @@
 import axios from "axios";
 
 // Di sini kita membuat instance dari axios
+const API_KEY = process.env.REACT_APP_TMDB_KEY;
 const tmdbInstance = axios.create({
   baseURL: "https://api.themoviedb.org/3",
   params: {
     // TODO: Jangan lupa masukkan API_KEY yang benarnya di sini yah !
-    api_key: "1e5238a16446199e1393210ccac016b4",
+    // api_key: "1e5238a16446199e1393210ccac016b4",
+    api_key: API_KEY,
   },
 });
 
